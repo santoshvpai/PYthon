1367e9f2c0149f752a34c951c1c419cedf0aeabf
Santosh Pai <39641675+santoshvpai@users.noreply.github.com>
2025-03-29 20:26:45 +0530

    L1-Print
    Print in python

---
+++ LearnPython1.py
@@ -0,0 +1,15 @@
+print("Namskara : Chandan")
+print("Happy Learning")
+
+print("------------------")
+print("Python is a general-purpose interpreted, interactive, object-oriented, and high-level programming language.")
+print("It was created by Guido van Rossum during 1985-1990.")
+print("Python source code is also available under the GNU General Public License (GPL).")
+print("Python is a general-purpose programming language, so it can be used for many things.")
+print("Python is used for web development, AI, machine learning, operating systems, mobile application development so on")
+print("Python is a high-level language, which means it is easy to read and write.")
+print("Python is an interpreted language, which means that it is not compiled like Java or C++.")
+print("Python is interactive, which means that you can interact with the interpreter directly.")
+print("Python is object-oriented, which means that it treats everything in the program as an object.")
+
+
